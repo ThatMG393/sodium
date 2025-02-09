c2cb1b29c4dd46554574ae75d88d519fe810ab2b
ThatMG393 <thatmg393@gmail.com>
2025-02-09 11:46:00 +0800

    Cache draw batches

--- common/src/main/java/net/caffeinemc/mods/sodium/client/render/chunk/region/RenderRegionManager.java
+++ common/src/main/java/net/caffeinemc/mods/sodium/client/render/chunk/region/RenderRegionManager.java
@@ -77,6 +77,7 @@ public class RenderRegionManager {
 
                     if (storage != null) {
                         storage.removeVertexData(renderSectionIndex);
+                        region.clearCachedBatchFor(pass);
                     }
 
                     BuiltSectionMeshParts mesh = chunkBuildOutput.getMesh(pass);
@@ -93,6 +94,7 @@ public class RenderRegionManager {
                 if (sorter instanceof SharedIndexSorter sharedIndexSorter) {
                     var storage = region.createStorage(DefaultTerrainRenderPasses.TRANSLUCENT);
                     storage.removeIndexData(renderSectionIndex);
+                    region.clearCachedBatchFor(DefaultTerrainRenderPasses.TRANSLUCENT);
                     storage.setSharedIndexUsage(renderSectionIndex, sharedIndexSorter.quadCount());
                 } else {
                     var storage = region.getStorage(DefaultTerrainRenderPasses.TRANSLUCENT);
@@ -137,6 +139,7 @@ public class RenderRegionManager {
             // Once invalidated the tessellation will be re-created on the next attempted use
             if (bufferChanged) {
                 region.refreshTesselation(commandList);
+                region.clearAllCachedBatches();
             }
 
             // Collect the upload results
@@ -167,6 +170,7 @@ public class RenderRegionManager {
 
         if (indexBufferChanged) {
             region.refreshIndexedTesselation(commandList);
+            region.clearCachedBatchFor(DefaultTerrainRenderPasses.TRANSLUCENT);
         }
 
         profiler.pop();
